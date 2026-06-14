9c52f203d6c608d4704eb0fb8233f92793fac0b0
dedrick irby <irb25001@byui.edu>
2026-06-13 23:05:38 -0600

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIAK1TOtw3513Hi5st-XpguJtJtz38lEc",
+  authDomain: "outdoorproject-f1398.firebaseapp.com",
+  projectId: "outdoorproject-f1398",
+  storageBucket: "outdoorproject-f1398.firebasestorage.app",
+  messagingSenderId: "746626660145",
+  appId: "1:746626660145:web:67e427d2489559c401a210",
+  measurementId: "G-CSGTQYSD80"
+};
